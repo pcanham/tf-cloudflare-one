5e1b07f760c69d6497e2791bb996c010af81eb76
Paul Canham <paul.canham@cambridge.org>
2024-11-27 11:08:42 +0000

    adding in example which cant be edited in console

--- gateway_policy_http_000_bypasses.tf
+++ gateway_policy_http_000_bypasses.tf
@@ -1,12 +1,31 @@
-resource "cloudflare_zero_trust_gateway_policy" "http_bypass_app_devtooling" {
+resource "cloudflare_zero_trust_gateway_policy" "http_bypass_app_devtooling00" {
   account_id  = var.cloudflare_account_id
   action      = "off"
-  description = "BUG REPORT - Do not inspect"
+  description = "BUG REPORT 00 - Do not inspect"
   enabled     = true
   filters     = ["http"]
-  name        = "BUG REPORT - Development Tooling"
+  name        = "BUG REPORT 00 - Development Tooling"
   precedence  = 999991
   traffic     = "any(app.ids[*] in {536 687 1238})"
+  rule_settings {
+    block_page_enabled                 = false
+    insecure_disable_dnssec_validation = false
+    ip_categories                      = false
+    notification_settings {
+      enabled = false
+    }
+  }
+}
+
+resource "cloudflare_zero_trust_gateway_policy" "http_bypass_app_devtooling01" {
+  account_id  = var.cloudflare_account_id
+  action      = "off"
+  description = "BUG REPORT 01 - Do not inspect"
+  enabled     = true
+  filters     = ["http"]
+  name        = "BUG REPORT 01 - Development Tooling"
+  precedence  = 999992
+  traffic     = "any(app.hosts_ids[*] in {536 687 1238})"
   rule_settings {
     block_page_enabled                 = false
     insecure_disable_dnssec_validation = false
@@ -24,7 +43,7 @@ resource "cloudflare_zero_trust_gateway_policy" "http_bypass_app_list" {
   enabled     = true
   filters     = ["http"]
   name        = "BUG REPORT - Bypass HTTPS Inspection"
-  precedence  = 999992
+  precedence  = 999993
   traffic     = "any(app.ids[*] in {683 707 657 658 571 712 720 729 787})"
   rule_settings {
     block_page_enabled                 = false
@@ -43,7 +62,7 @@ resource "cloudflare_zero_trust_gateway_policy" "http_do_not_inspect_m365" {
   enabled     = true
   filters     = ["http"]
   name        = "BUG REPORT - Microsoft 365 Auto Generated"
-  precedence  = 999993
+  precedence  = 999994
   traffic     = "any(app.ids[*] in {626 594 635 514 601 596 597 680})"
   rule_settings {
     block_page_enabled                 = false
@@ -62,7 +81,7 @@ resource "cloudflare_zero_trust_gateway_policy" "http_block_sec_risks" {
   enabled     = true
   filters     = ["http"]
   name        = "BUG REPORT - Block Security Risks"
-  precedence  = 999994
+  precedence  = 999995
   traffic     = "any(http.request.uri.security_category[*] in {80 117 131 151 153})"
   rule_settings {
     block_page_enabled                 = true
